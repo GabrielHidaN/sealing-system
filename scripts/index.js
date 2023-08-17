d1018bfd364bff02c1e56edf3f655dce41362324
GabrielHidaN <victorbielgv7@gmail.com>
2023-08-17 17:52:11 -0300

    commit 17/08

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
-const formEl = document.getElementById('form');
+const formEl = document.getElementById('form')
+
+const pswEl = document.getElementById('password-user');
+const pswCfmEl = document.getElementById('password-confirm');
 
 formEl.addEventListener('submit', event => {
     event.preventDefault();
@@ -6,5 +9,41 @@ formEl.addEventListener('submit', event => {
     const formData = new FormData(formEl);
     const data = Object.fromEntries(formData);
     console.log(data)
+
+    password();
+    
 })
+
+function password (){
+
+    if(pswEl.value != pswCfmEl.value){
+        alert('as senhas não coincidem')
+
+    }
+
+   
+
+    function verificaTamanhoMinimoSenha(senha, tamanhoMinimo) {
+        return senha.length >= tamanhoMinimo;
+      }
+    
+      
+      // Exemplo de uso
+      const senha1 = "senha123";
+      const senha2 = "abc";
+      const tamanhoMinimo = 6;
+      
+      if (verificaTamanhoMinimoSenha(senha1, tamanhoMinimo)) {
+        console.log("Senha válida!");
+      } else {
+        console.log("Senha muito curta!");
+      }
+      
+      if (verificaTamanhoMinimoSenha(senha2, tamanhoMinimo)) {
+        console.log("Senha válida!");
+      } else {
+        console.log("Senha muito curta!");
+      }
+    }
+
 
